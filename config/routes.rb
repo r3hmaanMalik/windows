4f5816c47bcfa82c380533ca0e103c93f87ddfc9
rehman <r3hmaan@hotmaio.com>
2017-04-01 20:23:42 -0700

    windows git start

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :products
+  get 'welcome/index'
+  root 'products#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
